1545b58574b767c4c38056eb542d1bdc899024bd
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-06-04 13:28:03 +0530

    [Ranjith] added AddressBook.java

---
+++ AddressBook.java
@@ -0,0 +1,18 @@
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class AddressBook {
+	public static void main(String[] args)
+    {
+        // Creating a Stream of Strings
+        Stream<String> stream = Stream.of("Ganesh", "Santhosh","Rekha", "Babu","Ranjith");
+
+        // Using Stream filter(Predicate predicate)
+        // to get a stream consisting of the
+        // elements ending with s
+        stream.filter(str -> str.startsWith("S"))
+        .forEach(System.out::println);
+    }
+}
